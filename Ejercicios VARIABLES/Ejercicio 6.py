e77ccb8e65fba42d19c3032af5f3b49a43cbc046
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:54:29 +0200

    Update Ejercicio 6.py

--- Ejercicios VARIABLES/Ejercicio 6.py
+++ Ejercicios VARIABLES/Ejercicio 6.py
@@ -1,6 +1,6 @@
 # Escribe un programa que pida una temperatura en grados Fahrenheit y que escriba esa temperatura en grados Celsius.
+# Recuerda que la relación entre grados Celsius (ºC) y grados Fahrenheit (ºF) es la siguiente: C = (F - 32) / 1,8
 
-# Se recuerda que la relación entre grados Celsius (ºC) y grados Fahrenheit (ºF) es la siguiente: C = (F - 32) / 1,8
 fahrenheit = float(input("Escribe una temperatura en ºF: "))
 
 celsius = float(round((fahrenheit - 32)/1.8, 3))
