9aa0169d77ae33c12a23afa57bb8272d11322d27
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:50:20 +0200

    Update Ejercicio 05.py

--- Ejercicios IF - ELIF - ELSE/Ejercicio 05.py
+++ Ejercicios IF - ELIF - ELSE/Ejercicio 05.py
@@ -1,10 +1,9 @@
 # Escribe un programa que pida dos números enteros y que calcule su división, escribiendo si la división es exacta o no.
 
 dividendo = int(input("Escribe un número entero: "))
-divisor = int(input("EScribe otro número entero: "))
+divisor = int(input("Escribe otro número entero: "))
 
 if dividendo % divisor == 0:
     print("La división es exacta. Cociente:", dividendo // divisor,)
 else:
-    print("La división no es exacta. Cociente:", dividendo //
-          divisor, "Resto:", dividendo % divisor,)
+    print("La división no es exacta. Cociente:", dividendo // divisor, "Resto:", dividendo % divisor,)
