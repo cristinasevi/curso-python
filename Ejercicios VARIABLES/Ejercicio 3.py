8d80b6fc07c424e97703569aa52078b7a1729503
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:52:56 +0200

    Update Ejercicio 3.py

--- Ejercicios VARIABLES/Ejercicio 3.py
+++ Ejercicios VARIABLES/Ejercicio 3.py
@@ -1,6 +1,6 @@
 # Escribe un programa que pida una distancia en pies y pulgadas y que escriba esa distancia en centímetros.
-
 # Se recuerda que un pie son 12 pulgadas y una pulgada son 2,54 cm.
+
 pies = float(input("Escribe una distancia en pies: "))
 pulgadas = float(input("Escribe una distancia en pulgadas: "))
 
