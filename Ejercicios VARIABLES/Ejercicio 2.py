ac996f97084495676c76cd2ed4a34d19ae42b273
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:52:41 +0200

    Update Ejercicio 2.py

--- Ejercicios VARIABLES/Ejercicio 2.py
+++ Ejercicios VARIABLES/Ejercicio 2.py
@@ -1,6 +1,6 @@
 # Escribe un programa que pida el peso (en kilogramos) y la altura (en metros) de una persona y que calcule su índice de masa corporal (imc).
+# Recuerda que el imc se calcula con la fórmula imc = peso / altura^2
 
-# Se recuerda que el imc se calcula con la fórmula imc = peso / altura^2
 peso = float(input("Introduzca su peso en kilogramos: "))
 altura = float(input("Introduzca su altura en metros: "))
 
