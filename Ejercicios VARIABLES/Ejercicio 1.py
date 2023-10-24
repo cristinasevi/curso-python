09ac5c76df73db1b801fb4d8adb5ddfb7bfb7baf
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:52:24 +0200

    Update Ejercicio 1.py

--- Ejercicios VARIABLES/Ejercicio 1.py
+++ Ejercicios VARIABLES/Ejercicio 1.py
@@ -1,6 +1,6 @@
 # Escribe un programa que pida dos números y que escriba su media aritmética.
+# Recuerda que la media aritmética de dos números es la suma de ambos números dividida por 2.
 
-# Se recuerda que la media aritmética de dos números es la suma de ambos números dividida por 2.
 numero1 = int(input("Escribe un número: "))
 numero2 = int(input("Escribe el segundo número: "))
 
