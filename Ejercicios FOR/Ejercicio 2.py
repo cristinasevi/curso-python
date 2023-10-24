c842d5633308a2449d4dd7f3054692d3f7741c8d
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:45:34 +0200

    Update Ejercicio 2.py

--- Ejercicios FOR/Ejercicio 2.py
+++ Ejercicios FOR/Ejercicio 2.py
@@ -1,8 +1,8 @@
 # Escribe un programa que pregunte cuántos números se van a introducir, pida esos números, y muestre un mensaje cada vez que un número no sea mayor que el primero.
 
 numeros = int(input("¿Cuántos números vas a introducir?: "))
-
 numero1 = int(input("Escribe un número: "))
+
 for i in range(numeros - 1):
     numero2 = int(input("Escribe otro número más grande que %d:" % numero1))
     if numero2 <= numero1:
