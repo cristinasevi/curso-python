4b156771818354b6952ae1f4038cb6bcf8d73426
cris <133981266+cristinasevi@users.noreply.github.com>
2023-10-24 21:54:57 +0200

    Update Ejercicio 8.py

--- Ejercicios VARIABLES/Ejercicio 8.py
+++ Ejercicios VARIABLES/Ejercicio 8.py
@@ -1,6 +1,6 @@
 # Escribe un programa que pida una cantidad y que escriba cuántas gruesas, docenas y unidades son.
+# Recuerda que una gruesa son 12 docenas.
 
-# Se recuerda que una gruesa son 12 docenas.
 cantidad = int(input("Escribe una cantidad: "))
 
 resultado_gruesa = int
@@ -9,5 +9,4 @@ resultado_unidades = int
 resultado_gruesa = (cantidad // 144)
 resultado_docenas = ((cantidad % 144)//12)
 resultado_unidades = ((cantidad % 144) % 12)
-print("Su cantidad es:", resultado_gruesa, "gruesa", resultado_docenas,
-      "docenas", resultado_unidades, "unidades")
+print("Su cantidad es:", resultado_gruesa, "gruesa", resultado_docenas, "docenas", resultado_unidades, "unidades")
